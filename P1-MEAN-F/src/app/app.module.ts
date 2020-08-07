223ddea4218b5f7850d9319343fe0fede6ac561f
kanfamPK <khanpham1995@gmail.com>
2020-08-07 15:08:38 +0700

    progessing 1

--- P1-MEAN-F/src/app/app.module.ts
+++ P1-MEAN-F/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularMaterialModule } from './angular-material.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
