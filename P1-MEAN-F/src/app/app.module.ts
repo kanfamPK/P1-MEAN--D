61a0d458e871d2afa5567f5678b1ab466974cf14
kanfamPK <khanpham1995@gmail.com>
2020-08-08 00:16:42 +0700

    processing 2

--- P1-MEAN-F/src/app/app.module.ts
+++ P1-MEAN-F/src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularMaterialModule } from './angular-material.module';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    NgbModule,
   ],
   exports: [
   ],
