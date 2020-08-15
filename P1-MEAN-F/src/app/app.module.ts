fb1ff27f6a574f71cff0c6ffca12c68059f911ba
kanfamPK <khanpham1995@gmail.com>
2020-08-15 11:08:34 +0700

    commit 3

--- P1-MEAN-F/src/app/app.module.ts
+++ P1-MEAN-F/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AngularMaterialModule } from './angular-material.module';
+import { AngularMaterialModule } from './modules/angular-material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { PostListComponent } from './components/posts/post-list/post-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
